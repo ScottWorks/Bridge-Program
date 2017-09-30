311fd512f49f5a27803c3e85ee4bdfc68165d6fb
ScottWorks <cescott.ieee@gmail.com>
2017-09-30 10:51:54 -0400

    refactored code

--- Week3/HWSubmission/ces440_hw3_q6.cpp
+++ Week3/HWSubmission/ces440_hw3_q6.cpp
@@ -1,55 +1,35 @@
-/*
-Author: Courtney E. Scott
-Date: 09/24/2017
-
-Task: 
-Write	a	program	that	asks	the	user	to	input	a	positive	integer	n,	and	print	all	of	the	numbers	
-from	1	to	n	that	have	more	even	digits	than	odd	digits.		
-*/
-
 #include <iostream>
-using namespace std; 
+using namespace std;
 
-int main(){
-    int inputVal, oddCount, evenCount, count, countSort, currVal; 
-
-    cout<<"Enter a positive integer:    ";
-    cin>>inputVal;
-
-    // starts conting from 1 to n
-    for(count=0; count<=inputVal; count++){
-        
-        // if flag is raised exit loop
-        bool flag=false;
-        currVal=0;
-        oddCount=0;
-        evenCount=0;
-        countSort=count;
-        while(flag==false){  
-
-            // break down input val
-            currVal=countSort%10;
-            countSort=countSort/10;      
-            
-            // print count and exit only if break down of input is finished and eventCount>oddCount
-            if(countSort<=0 && currVal<=0 && evenCount>oddCount){
-                cout<<count<<endl;      
-                flag=true;
-            }   
-
-            // exit only if break down of input is finished and Oddcount>=evenCount
-            else if(countSort<=0 && currVal<=0 && evenCount<=oddCount){
-                flag=true;          
-                }
-
-            else if(currVal%2==1){
-            oddCount++;           
+int main() {
+   int input, counter, currDig, odd, even, newCounter;
+   
+   cout<<"Please enter a positive integer"<<endl;
+   cin>>input;
+   
+   for(counter=1; counter<=input; counter++){
+       currDig=0;
+       even = 0;
+       odd = 0;
+       newCounter = counter;
+       
+       while(newCounter > 0){
+           currDig = newCounter % 10;
+           newCounter = newCounter / 10;
+           
+           if(currDig % 2 == 0){
+               even++;
             }
+       
+          else if(currDig % 2 == 1){
+               odd++;
+            }
+       }
 
-            else if(currVal%2==0){
-            evenCount++;            
-            }                       
-        }            
-    }
-    return 0;
+       if(even > odd){
+           cout<<counter<<endl;
+        }
+   }
+
+   return 0;
 }
